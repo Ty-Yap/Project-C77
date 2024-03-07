72e00dff8a92f7da53d822871106bf634f5bc5d1
Ty-Yap <155054225+Ty-Yap@users.noreply.github.com>
2024-03-07 19:22:26 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var first_name = "Tyler";
+var last_name = "yap"
+var full_name = first_name.concat(last_name);
+console.log(full_name)
+var length_of_full_name = full_name.length;
+the_variable_the_holds_the_whole_string.replace
